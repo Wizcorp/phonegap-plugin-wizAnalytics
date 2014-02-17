20d738dd57f543c23a3b0e7f5f1f34368fb06348
aogilvie <aogilvie@wizcorp.jp>
2014-02-17 15:34:29 +0900

    updated ios project, added plugin.xml for plugman

---
+++ platforms/ios/www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js
@@ -0,0 +1,29 @@
+/* WizAnalyticsPlugin for cordova
+*
+ * @author Ally Ogilvie  
+ * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
+ * @file - WizAnalyticsPlugin.js
+ * @about - Access WizAnalytics SDKs through JavaScript
+ *
+ *
+*/
+
+var wizAnalytics = {
+
+    event : {
+        log : function (eventName, options) {
+            cordova.exec(null, null, "WizAnalyticsPlugin", "logEvent", [eventName, options]);
+        }
+    },
+    
+    screen  : {
+        log : function (screenName, options) {
+            cordova.exec(null, null, "WizAnalyticsPlugin", "logScreen", [screenName, options]);
+        }
+    },
+
+    handleOpenURL : function(url) {
+            cordova.exec(null, null, "WizAnalyticsPlugin", "handleOpenURL", [url]);
+    }
+
+};
