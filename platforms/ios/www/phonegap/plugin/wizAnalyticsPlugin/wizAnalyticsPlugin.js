47364e8068476bf1064acf45aa0376433ef4ddba
aogilvie <aogilvie@wizcorp.jp>
2014-02-17 15:48:15 +0900

    updated JS files for plugman compatibility

--- platforms/ios/www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js
+++ platforms/ios/www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js
@@ -1,5 +1,5 @@
-/* WizAnalyticsPlugin for cordova
-*
+cordova.define("jp.wizcorp.phonegap.plugin.wizAnalytics.wizAnalyticsPlugin", function(require, exports, module) {/* WizAnalyticsPlugin for cordova
+ *
  * @author Ally Ogilvie  
  * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
  * @file - WizAnalyticsPlugin.js
@@ -8,22 +8,24 @@
  *
 */
 
-var wizAnalytics = {
-
-    event : {
-        log : function (eventName, options) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "logEvent", [eventName, options]);
-        }
-    },
-    
-    screen  : {
-        log : function (screenName, options) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "logScreen", [screenName, options]);
-        }
-    },
-
-    handleOpenURL : function(url) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "handleOpenURL", [url]);
-    }
-
-};
+	var wizAnalytics = {
+	
+	    event : {
+	        log : function (eventName, options) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "logEvent", [eventName, options]);
+	        }
+	    },
+	    
+	    screen  : {
+	        log : function (screenName, options) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "logScreen", [screenName, options]);
+	        }
+	    },
+	
+	    handleOpenURL : function(url) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "handleOpenURL", [url]);
+	    }
+	
+	};
+    module.exports = wizAnalytics;
+});
--- www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js
+++ www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js
@@ -1,29 +1,30 @@
 /* WizAnalyticsPlugin for cordova
-*
+ *
  * @author Ally Ogilvie  
- * @copyright WizCorp Inc. [ Incorporated Wizards ] 2012
+ * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
  * @file - WizAnalyticsPlugin.js
  * @about - Access WizAnalytics SDKs through JavaScript
  *
  *
 */
 
-var wizAnalytics = {
-
-    event : {
-        log : function (eventName, options) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "logEvent", [eventName, options]);
-        }
-    },
-    
-    screen  : {
-        log : function (screenName, options) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "logScreen", [screenName, options]);
-        }
-    },
-
-    handleOpenURL : function(url) {
-            cordova.exec(null, null, "WizAnalyticsPlugin", "handleOpenURL", [url]);
-    }
-
-};
+	var wizAnalytics = {
+	
+	    event : {
+	        log : function (eventName, options) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "logEvent", [eventName, options]);
+	        }
+	    },
+	    
+	    screen  : {
+	        log : function (screenName, options) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "logScreen", [screenName, options]);
+	        }
+	    },
+	
+	    handleOpenURL : function(url) {
+	            cordova.exec(null, null, "WizAnalyticsPlugin", "handleOpenURL", [url]);
+	    }
+	
+	};
+    module.exports = wizAnalytics;
