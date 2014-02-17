c8e5ce3c498cf9c23bab2187e3f31437ec11f178
aogilvie <aogilvie@wizcorp.jp>
2014-02-17 17:48:35 +0900

    final commits to sample project, added example JS events and index.html

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/jp.wizcorp.phonegap.plugin.wizAnalytics/www/phonegap/plugin/wizAnalyticsPlugin/wizAnalyticsPlugin.js",
+        "id": "jp.wizcorp.phonegap.plugin.wizAnalytics.wizAnalyticsPlugin",
+        "clobbers": [
+            "window.wizAnalytics"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "jp.wizcorp.phonegap.plugin.wizAnalytics": "1.0.0"
+}
 // BOTTOM OF METADATA
 });
