1f7ed2eae65c2a8e0191ad5cda56145d524fa6de
aogilvie <aogilvie@wizcorp.jp>
2014-02-17 15:31:46 +0900

    added Flurry module files back in

---
+++ platforms/ios/HelloCordova/Plugins/WizAnalyticsPlugin/vendor/ModuleFlurry.h
@@ -0,0 +1,13 @@
+/* wizAnalytics - Flurry Module
+ *
+ * @author Ally Ogilvie
+ * @copyright WizCorp Inc. [ Incorporated Wizards ] 2012
+ * @file ModuleFlurry.h for iOS
+ *
+ *
+ */
+
+#import "WizAnalyticsVendorModule.h"
+
+@interface ModuleFlurry : NSObject <WizAnalyticsVendorModule>
+@end
---
+++ platforms/ios/HelloCordova/Plugins/WizAnalyticsPlugin/vendor/ModuleFlurry.m
@@ -0,0 +1,49 @@
+/* wizAnalytics - Flurry Module
+ *
+ * @author Ally Ogilvie
+ * @copyright WizCorp Inc. [ Incorporated Wizards ] 2012
+ * @file ModuleFlurry.m for iOS
+ *
+ *
+ */
+
+#import "ModuleFlurry.h"
+#import "Flurry.h"
+
+@interface ModuleFlurry ()
+@property (nonatomic, retain) NSString *flurryAPIKey;
+@end
+
+@implementation ModuleFlurry
+
+- (void)dealloc
+{
+    self.flurryAPIKey = nil;
+    [super dealloc];
+}
+
+#pragma mark - Required WizAnalyticsVendorModule protocol methods
+
+- (id)initWithOptions:(NSDictionary *)options
+{
+    if ((self = [super init])) {
+        self.flurryAPIKey = [options objectForKey:@"FlurryKey"];
+    }
+    return self;
+}
+
+- (void)startSession 
+{
+    NSLog(@"FLURRY START SESSION %@", _flurryAPIKey);
+    [Flurry startSession:_flurryAPIKey];
+}
+
+#pragma mark - Optional WizAnalyticsVendorModule protocol methods
+
+- (void)logEvent:(NSString *)eventName withExtraMetadata:(NSDictionary *)extraMetadata
+{
+    NSLog(@"FLURRY LOG EVENT %@ DATA %@", eventName, extraMetadata);
+    [Flurry logEvent:eventName withParameters:extraMetadata];
+}
+
+@end
